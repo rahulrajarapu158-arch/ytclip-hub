76a0bf9d18cc686bf20a715cf3c752cafde692f1
rahulrajarapu158-arch <rahulrajarapu15@gmail.com>
2026-09-15 16:17:51 +0000

    feat: navigate to Browse>Extensions tab from onboarding

--- app/src/main/java/eu/kanade/tachiyomi/ui/more/OnboardingScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/more/OnboardingScreen.kt
@@ -50,6 +50,14 @@ class OnboardingScreen : Screen() {
             },
             onNavigateToExtensions = {
                 finishOnboarding()
+                val scope = rememberCoroutineScope()
+                scope.launch {
+                    navigator.popUntilRoot()
+                    val homeScreen = navigator.lastItem
+                    if (homeScreen is HomeScreen) {
+                        homeScreen.openTab(HomeScreen.Tab.Browse(toExtensions = true))
+                    }
+                }
             },
         )
     }
