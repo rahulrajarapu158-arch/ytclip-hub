f337b101dc15353fba17e038a29edeab9f62927c
rahulrajarapu158-arch <rahulrajarapu15@gmail.com>
2026-09-15 15:43:12 +0000

    fix: remove verticalScroll from ExtensionsStep that caused crash in onboarding

--- app/src/main/java/eu/kanade/presentation/more/onboarding/ExtensionsStep.kt
+++ app/src/main/java/eu/kanade/presentation/more/onboarding/ExtensionsStep.kt
@@ -5,8 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -49,9 +48,7 @@ internal class ExtensionsStep(
         val clipboardManager = LocalClipboardManager.current
 
         Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .verticalScroll(rememberScrollState()),
+            modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.padding.medium),
         ) {
             Text(
